630058de9e0190adbb1493ec39b7e20c7368b0ef
Ziran Gong <gzr2008@vip.qq.com>
2018-09-18 20:18:33 -0400

    Update to 016
    Update to 016

--- Python3/004_Median_of_Two_Sorted_Arrays.py
+++ Python3/004_Median_of_Two_Sorted_Arrays.py
@@ -1,10 +1,9 @@
 #!usr/bin/env python
 # -*- coding:utf-8 -*-
-
-
 '''
 There are two sorted arrays nums1 and nums2 of size m and n respectively. Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
 '''
+
 
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
--- Python3/009_Palindrome_Number.py
+++ Python3/009_Palindrome_Number.py
@@ -1,7 +1,5 @@
 #!usr/bin/env python
 # -*- coding:utf-8 -*-
-
-
 '''
 Determine whether an integer is a palindrome. Do this without extra space.
 
@@ -13,6 +11,7 @@ You could also try reversing an integer. However, if you have solved the problem
 
 There is a more generic way of solving this problem.
 '''
+
 
 class Solution(object):
     def isPalindrome(self, x):
